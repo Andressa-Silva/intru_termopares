eb3bf8c940be6549ae77c774273d9992d96e279e
Andressa-Silva <69591988+Andressa-Silva@users.noreply.github.com>
2022-11-25 19:42:14 -0300

    Update thermocouples.py

--- thermocouples.py
+++ thermocouples.py
@@ -4,11 +4,11 @@ from math import e
 
 def thermocouple_typeB (): 
     temperature = 0
-    aux_soma = 0
-    points = []
+    aux_soma    = 0
+    points      = []
 
-    coefficient_B = [ 0.000000000000*pow(10,0), -0.246508183460*pow(10,-3),   0.590404211710*pow(10,-5),
-                     -0.132579316360*pow(10,-8), 0.156682919010*pow(10,-11), -0.169445292400*pow(10,-14),
+    coefficient_B = [ 0.000000000000*pow(10,0),  -0.246508183460*pow(10,-3),    0.590404211710*pow(10,-5),
+                     -0.132579316360*pow(10,-8),  0.156682919010*pow(10,-11),  -0.169445292400*pow(10,-14),
                       0.629903470940*pow(10,-14) ]
 
     while temperature <= 300:
@@ -22,12 +22,12 @@ def thermocouple_typeB ():
 
 def thermocouple_typeE ():
     temperature = 0
-    aux_soma = 0
-    points = []
+    aux_soma    = 0
+    points      = []
     
-    coefficient_E = [ 0.000000000000*pow(10,0),    0.586655087100*pow(10,-1),  0.450322755820*pow(10,-4),
-                      0.289084072120*pow(10,-7),  -0.330568966520*pow(10,-9),  0.650244032700*pow(10,-12),
-                     -0.191974955040*pow(10,-15), -0.125366004970*pow(10,-17), 0.214892175690*pow(10,-20),
+    coefficient_E = [ 0.000000000000*pow(10,0),    0.586655087100*pow(10,-1),   0.450322755820*pow(10,-4),
+                      0.289084072120*pow(10,-7),  -0.330568966520*pow(10,-9),   0.650244032700*pow(10,-12),
+                     -0.191974955040*pow(10,-15), -0.125366004970*pow(10,-17),  0.214892175690*pow(10,-20),
                      -0.143880417820*pow(10,-23),  0.359608994810*pow(10,-27)  ]       
 
     while temperature <= 300:
@@ -41,8 +41,8 @@ def thermocouple_typeE ():
 
 def thermocouple_typeJ ():
     temperature = 0
-    aux_soma = 0
-    points = []
+    aux_soma    = 0
+    points      = []
     
     coefficient_J = [ 0.000000000000*pow(10,0),    0.503811878150*pow(10,-1),  0.304758369300*pow(10,-4), 
                      -0.856810657200*pow(10,-7),   0.132281952950*pow(10,-9), -0.170529583370*pow(10,-12), 
@@ -59,8 +59,8 @@ def thermocouple_typeJ ():
     
 def thermocouple_typeK ():
     temperature = 0
-    aux_soma = 0
-    points = []
+    aux_soma    = 0
+    points      = []
     
     a_0 = 0.118597600000*pow(10,0)
     a_1 = -0.118343200000*pow(10,-3)
@@ -70,7 +70,7 @@ def thermocouple_typeK ():
     coefficient_K = [-0.176004136860*pow(10,-1),   0.389212049750*pow(10,-1),   0.185587700320*pow(10,-4), 
                      -0.994575928740*pow(10,-7),   0.318409457190*pow(10,-9),  -0.560728448890*pow(10,-12),
                       0.560750590590*pow(10,-15), -0.320207200030*pow(10,-18),  0.971511471520*pow(10,-22), 
-                     -0.121047212750*pow(10,-25) ]
+                     -0.121047212750*pow(10,-25)   ]
 
     while temperature <= 300:
         if temperature == 0:
@@ -89,8 +89,8 @@ def thermocouple_typeK ():
 
 def thermocouple_typeN ():
     temperature = 0
-    aux_soma = 0
-    points = []
+    aux_soma    = 0
+    points      = []
     
     coefficient_N = [ 0.000000000000*pow(10,0),    0.259293946010*pow(10,-1),   0.157101418800*pow(10,-4), 
                       0.438256272370*pow(10,-7),  -0.252611697940*pow(10,-9),   0.643118193390*pow(10,-12), 
@@ -109,8 +109,8 @@ def thermocouple_typeN ():
 
 def thermocouple_typeR ():
     temperature = 0
-    aux_soma = 0
-    points = []
+    aux_soma    = 0
+    points      = []
     
     coefficient_R = [ 0.000000000000*pow(10,0),    0.528961729765*pow(10,-2),   0.139166589782*pow(10,-4), 
                      -0.238855693017*pow(10,-7),   0.356916001063*pow(10,-10), -0.462347666298*pow(10,-13), 
@@ -128,8 +128,8 @@ def thermocouple_typeR ():
 
 def thermocouple_typeS ():
     temperature = 0
-    aux_soma = 0
-    points = []
+    aux_soma    = 0
+    points      = []
     
     coefficient_S = [ 0.000000000000*pow(10,0),    0.540313308631*pow(10,-2),   0.125934289740*pow(10,-4), 
                      -0.232477968689*pow(10,-7),   0.322028823036*pow(10,-10), -0.331465196389*pow(10,-13),
@@ -146,8 +146,8 @@ def thermocouple_typeS ():
 
 def thermocouple_typeT ():
     temperature = 0
-    aux_soma = 0
-    points = []
+    aux_soma    = 0
+    points      = []
     
     coefficient_T = [ 0.000000000000*pow(10,0),   0.387481063640*pow(10,-1),   0.332922278800*pow(10,-4),  
                       0.206182434040*pow(10,-6), -0.218822568460*pow(10,-8),   0.109968809280*pow(10,-10), 
